98b0165ebbff2d5789a206bff5c9959f4ac885be
Joetib <otiboatengjoe@gmail.com>
2020-10-10 18:12:43 +0000

    Added CourseListView with basic test

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('pages.urls')),
+    path('courses/', include('course.urls', namespace="course")),
 ]
 
 if settings.DEBUG:
--- course/views.py
+++ course/views.py
@@ -1,3 +1,11 @@
+from typing import List
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from . import models
 # Create your views here.
+
+class CourseListView(ListView):
+    model = models.Course
+    paginate_by = 12
+    context_object_name = "courses"
+    template_name = "course/course_list.html"
