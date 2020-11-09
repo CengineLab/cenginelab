36b0ac7b0aee2ecebb1c2fcea296d8ca26d4bc29
Joetib <otiboatengjoe@gmail.com>
2020-11-09 13:22:41 +0000

    Added bootstrap

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +15,4 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-] + urlpatterns
+] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
