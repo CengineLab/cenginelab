d15b16abd1f69b7c90d65362e9a1d2f85047d7c2
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2020-11-23 00:15:20 +0000

    Made changes in the front-end

---
+++ static/js/base.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("open").style.display = "none";
+    document.getElementById("close").style.display = "block";
+  }
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("open").style.display = "block";
+    document.getElementById("close").style.display = "none";
+  }
